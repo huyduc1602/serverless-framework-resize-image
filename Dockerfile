571252f7be80c88e0ea41d346e5d5fb5b0d4f873
IDS-V-Duchh <duchh@vn.ids.jp>
2024-01-25 15:13:21 +0700

    Update Dockerfile and handler.py, add dependencies and improve image resizing

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-# Sử dụng hình ảnh lambci/lambda:build-python3.8 làm hình ảnh cơ sở
+# Sử dụng hình ảnh lambci/lambda:build-python3.8 làm base image
 FROM lambci/lambda:build-python3.8
 
-# Cài đặt Pillow vào thư mục python/lib/python3.8/site-packages/
+# Cài đặt các gói phụ thuộc
+RUN yum install -y libjpeg-turbo-devel zlib-devel libtiff-devel freetype-devel libwebp-devel openjpeg2-devel
+
+# Cài đặt Pillow vào thư mục python
 RUN pip install Pillow -t python/
 
 # Tạo một file zip chứa thư mục python
--- handler.py
+++ handler.py
@@ -1,7 +1,10 @@
-import PIL
+import base64
+import sys
+from PIL import Image
 import io
 import json
 import boto3
+from datetime import datetime
 
 s3 = boto3.client('s3')
 
@@ -12,7 +15,8 @@ def resize_image(event, context):
         image_data = body['image_data']
 
         # Xử lý hình ảnh
-        image = PIL.Image.open(io.BytesIO(image_data))
+        decoded_image_data = base64.b64decode(image_data)
+        image = Image.open(io.BytesIO(decoded_image_data))
         new_width = 100
         new_height = int((float(image.size[1]) * float(new_width / float(image.size[0]))))
         resized_image = image.resize((new_width, new_height))
@@ -22,10 +26,12 @@ def resize_image(event, context):
         resized_image.save(resized_image_bytes, format='JPEG')
         resized_image_data = resized_image_bytes.getvalue()
 
+        image_name = datetime.now().strftime("%Y%m%d%H%M%S") + '.jpg'
+
         # Lưu hình ảnh đã xử lý vào S3
         s3.put_object(
             Bucket='duchh-pj-internal-resize-image',
-            Key='resized/image.jpg',
+            Key='resized/'+ image_name,
             Body=resized_image_data,
             ContentType='image/jpeg'
         )
@@ -35,7 +41,10 @@ def resize_image(event, context):
             "body": json.dumps({"message": "Resize hình ảnh thành công"})
         }
     except Exception as e:
+        error_line = sys.exc_info()[-1].tb_lineno
+        error_message = str(e)
+        print(f"Error at line {error_line}: {error_message}")
         return {
             "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
+            "body": json.dumps({"error": error_message})
         }
